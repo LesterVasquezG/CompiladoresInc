5a69c775db36549e0174829bab9c0aa2a56c9500
kolverar <kevincristian@outlook.com>
2020-05-29 13:15:39 -0500

    Ejercicio 1

---
+++ ejercicio_1/ejercicio_1.py
@@ -0,0 +1,13 @@
+fuente = open("fuente.txt", "r")
+salida = open("salida.txt", "w")
+
+lineasFuente = fuente.readlines()
+print(lineasFuente)
+
+lineasValidas = []
+
+for linea in lineasFuente:
+    pass
+
+fuente.close()
+salida.close()
